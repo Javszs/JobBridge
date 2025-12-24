1ea2eabf74be981bc77e8a858b5e60b143c124f4
Jayve Espira <qjespira@tip.edu.ph>
2025-12-24 16:43:20 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'JobBridge',
+  webDir: 'dist'
+};
+
+export default config;
